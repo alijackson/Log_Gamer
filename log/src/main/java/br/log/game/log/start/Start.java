df5d09fa71c14512715b7b4353b51e2916482a08
Jackson <jackson3.motta@hotmail.com>
2019-09-19 19:50:36 -0300

    Estruturo principal 	Estrutura principal do Spring.

---
+++ log/src/main/java/br/log/game/log/start/Start.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.log.game.log.start;
+
+import br.log.game.log.model.Game;
+import br.log.game.log.model.Kill;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ *
+ * @author alijackson.msilva
+ */
+@Configuration
+public class Start {
+    @Bean
+    public void readFile() throws IOException{
+        String linha = "";
+        int cont = 1;
+        try (BufferedReader log =  
+                new BufferedReader(new FileReader("log\\games.log"))){
+            Game game = new Game("game_1");
+            while(log.ready()){
+                linha = log.readLine();
+                game.log(linha);
+            }
+        }
+    }
+}
+
